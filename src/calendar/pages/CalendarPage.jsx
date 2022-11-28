223406c06e606ece86218fc52b5fad32d90bde88
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-11-27 20:22:45 -0500

    react big calendar y configuraciones del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,57 @@
-import { Navbar } from '../components/Navbar';
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import { addHours } from 'date-fns';
+import { Navbar } from '../';
+import { localizer, getMessagesES } from '../../helpers';
+
+
+const events = [
+  {
+    title: 'Cumpleaños de la empresa',
+    notes: 'Some notes',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Kevin',
+    },
+  },
+];
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+    console.log(`🚀 ~ file: CalendarPage.jsx ~ line 26 ~ eventStyleGetter ~ event, start, end, isSelected`, event, start, end, isSelected);
+
+    const style = {
+      backgroundColor: '#367cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    };
+
+    return {
+      style,
+    };
+
+  };
+
   return (
     <>
       <Navbar />
+      <Calendar
+        culture='es'
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={ { height: 'calc( 100vh - 80px )' } }
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
+      />
     </>
   );
 };
