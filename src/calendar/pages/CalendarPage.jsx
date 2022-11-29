cc825fd0d3ef9f693ae929c2721319b40920b589
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-11-28 21:18:51 -0500

    escuchar eventos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -4,6 +4,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns';
 import { CalendarEvent, Navbar } from '../';
 import { localizer, getMessagesES } from '../../helpers';
+import { useState } from 'react';
 
 
 const events = [
@@ -22,9 +23,9 @@ const events = [
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
   const eventStyleGetter = (event, start, end, isSelected) => {
-
-
     const style = {
       backgroundColor: '#367cf7',
       borderRadius: '0px',
@@ -38,21 +39,38 @@ export const CalendarPage = () => {
 
   };
 
+  const onDoubleClick = (event) => {
+    console.log(`🚀 ~ file: CalendarPage.jsx ~ line 42 ~ onDoubleClick ~ event`, event);
+  };
+
+  const onSelectEvent = (event) => {
+    console.log(`🚀 ~ file: CalendarPage.jsx ~ line 47 ~ onSelectEvent ~ event`, event);
+  };
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+    setLastView(event);
+  };
+
   return (
     <>
       <Navbar />
       <Calendar
         culture='es'
-        localizer={ localizer }
-        events={ events }
+        localizer={localizer}
+        events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
-        style={ { height: 'calc( 100vh - 80px )' } }
-        messages={ getMessagesES() }
-        eventPropGetter={ eventStyleGetter }
-        components={ {
+        style={{ height: 'calc( 100vh - 80px )' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
           event: CalendarEvent,
-        } }
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChanged}
       />
     </>
   );
