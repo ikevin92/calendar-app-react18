e7f8ca9f551bbd709ba977944d86198e37c189d6
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-12-02 17:55:13 -0500

    preparar la creacion del nuevo evento

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { useState } from 'react';
-import { CalendarEvent, CalendarModal, Navbar } from '../';
+import { CalendarEvent, CalendarModal, FabAddNew, Navbar } from '../';
 import { getMessagesES, localizer } from '../../helpers';
 import { useCalendarStore, useUiStore } from '../../hooks';
 
@@ -64,6 +64,7 @@ export const CalendarPage = () => {
       />
 
       <CalendarModal />
+      <FabAddNew />
     </>
   );
 };
